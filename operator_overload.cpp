b07cc9ef733b4153c4e82c17dbea399f76ba3da2
IshanOnGit <ishansidhaye888@gmail.com>
2025-04-11 16:55:09 +0530

    initial commit

--- operator_overload.cpp
+++ operator_overload.cpp
@@ -1,18 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class A{
-    public:
-    string a1 = " Hello";
+class MinusOverload {
+private:
+    int a;
+    int b;
+
+public:
+    void Distance()
+    {
+        a = 0;
+        b = 0;
+    }
+
+    MinusOverload(int f, int i) //Parameterized Constructor
+    {
+        int c;
+        a = f;
+        b = i;
+        c = a - b;
+        cout << "\nC:" << c;
+    }
+
     void display()
     {
-        cout<<a1;
+        cout << "A: " << a << " B:" << b << endl;
+    }
+
+    MinusOverload operator-()
+    {
+        a = -a;
+        b = -b;
+        return MinusOverload(a, b);
     }
 };
-class B{
-    public:
-    string a2 = "World";
-    void con(){
-        cout<<a2;
-    }
-};
-//void operator +
+
+int main()
+{
+    MinusOverload M1(6, 8), M2(-3, -4);
+    -M1;
+    M1.display();
+    -M2;
+    M2.display();
+    return 0;
+}
